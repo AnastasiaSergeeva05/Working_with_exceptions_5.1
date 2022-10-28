7eaa27b408059accc77cc8efeffc614e2b90d427
AnastasiaSergeeva05 <gotsenko_nasty@mail.ru>
2022-10-28 14:13:01 +0300

    задача 5.1.2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(checkData("dfgrewe", "3r465", "f4erwd"));
+        System.out.println(checkData("logon","password","password"));
+
+
+    }
+
+    public static boolean checkData(String login, String password, String confPassword) {
+        try {
+            return Data.userData(login, password, confPassword);
+        } catch (WrongLoginException e) {
+            System.out.println("Ошибка логина" + e.getMessage());
+            return false;
+
+        }catch (WrongPasswordException e){
+            System.out.println("Ошибка пароля" + e.getMessage());
+            return false;
+        }
+
+
     }
 }
